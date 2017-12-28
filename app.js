55e11eac1486e41dde2b7a042a7be840e8fc99c0
Moquo <mail@moquo.de>
2017-12-28 17:49:08 +0100

    Added basic stuff for discord.js bot

--- app.js
+++ app.js
@@ -4,3 +4,12 @@
 
 // Get the configuration
 const config = require('./config.json')
+
+// discord.js library
+const Discord = require('discord.js');
+
+// Client
+const client = new Discord.Client();
+
+// Login with bot token
+client.login(config.token);
